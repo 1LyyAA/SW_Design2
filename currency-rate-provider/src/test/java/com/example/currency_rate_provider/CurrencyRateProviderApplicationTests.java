5c996dd73587d8c53ca67781869fa6f8df6cffb6
1LyyAA <ilyaiudov678@gmail.com>
2026-05-17 21:46:19 +0300

    HW4 part 1

--- currency-rate-provider/src/test/java/com/example/currency_rate_provider/CurrencyRateProviderApplicationTests.java
+++ currency-rate-provider/src/test/java/com/example/currency_rate_provider/CurrencyRateProviderApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.currency_rate_provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "zookeeper.registration.enabled=false")
 class CurrencyRateProviderApplicationTests {
 
 	@Test
